484d91c99fab2b80f420d88bb83712fc5c91d617
Akintunde Teslim Akinmuyiwa <akinmuyiwateslim@gmail.com>
2025-05-07 17:16:41 +0100

    adding the frontend

--- config/allowedOrigin.js
+++ config/allowedOrigin.js
@@ -1,2 +1,5 @@
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://task-frontend-tawny.vercel.app",
+];
 module.exports = allowedOrigins;
